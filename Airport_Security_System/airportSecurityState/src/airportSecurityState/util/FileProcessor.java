548f5527b05b01566e533e44e7cd83bc9ea41145
Abhijit Bhandarkar <abhanda4@binghamton.edu>
2018-03-16 11:37:47 -0400

    Airport Security System

---
+++ Airport_Security_System/airportSecurityState/src/airportSecurityState/util/FileProcessor.java
@@ -0,0 +1,39 @@
+package airportSecurityState.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import airportSecurityState.util.MyLogger.DebugLevel;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileProcessor {
+
+	private String inputFile;
+	private BufferedReader br;
+
+	/**
+	 * Constructs a new FileProcessor which reads from the file specified by
+	 * command line input
+	 * 
+	 * @param inputFile
+	 *            name of the file to read
+	 * @throws FileNotFoundException
+	 */
+	public FileProcessor(String inputFile) throws FileNotFoundException {
+		this.inputFile = inputFile;
+		initializeBufferReader();
+		MyLogger.writeMessage("FileProcessor Constructor", DebugLevel.CONSTRUCTOR);
+	}
+
+	private void initializeBufferReader() throws FileNotFoundException {
+		br = new BufferedReader(new FileReader(inputFile));
+	}
+
+	public String readLine() throws IOException {
+		return br.readLine();
+
+	}
+
+}
